7971081c2b74b1552a3f545b7d8822039fbfeac4
wythe_wee@outlook.com <wythe_wee@outlook.com>
2016-03-17 22:25:29 +0800

    no message

--- app/src/main/java/com/example/vonlion/kupao/Histroy_chart.java
+++ app/src/main/java/com/example/vonlion/kupao/Histroy_chart.java
@@ -419,7 +419,7 @@ public class Histroy_chart extends Activity implements AMap.OnMapScreenShotListe
         }
         public Bitmap newBitmap(Bitmap bit1,Bitmap bit2){
             int width = bit1.getWidth();
-            int height = bit1.getHeight() + bit2.getHeight();
+            int height = bit2.getHeight();
             //创建一个空的Bitmap(内存区域),宽度等于第一张图片的宽度，高度等于两张图片高度总和
             Bitmap bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
             //将bitmap放置到绘制区域,并将要拼接的图片绘制到指定内存区域
