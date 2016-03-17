2770791405ebc42dae810067e97d0542769c36af
lihaof <vonlion.f@gmail.com>
2016-03-17 21:22:33 +0800

    增加了我的好友界面，可以把好友的listview放进去了

--- app/src/main/java/com/example/vonlion/kupao/MainActivity.java
+++ app/src/main/java/com/example/vonlion/kupao/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.Window;
 
 
 public class MainActivity extends Activity{
